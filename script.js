21d4d529ee747a8bfb8a184aecb6f744a04c4d76
Sahibjot Saggu <sahibjot@live.ca>
2014-11-01 22:27:08 -0400

    New Updated code.

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ $(document).ready(function() {
 	var list = document.getElementById("course_list");
 	$("#add_course").click(function() {
 		var course_code = document.getElementById('course_code').value;
+		document.getElementById("course_code").value = "";
 
 		// error_checking for 'course_code' here:
-
-		
 
 		var entry = document.createElement('li');
 		entry.appendChild(document.createTextNode(course_code));
 		list.appendChild(entry);
+	});
+
+	for (var x = 1; x <= 7; x++) {
+		document.getElementById("from_hour" + x).disabled = true;
+		document.getElementById("from_min" + x).disabled = true;
+		document.getElementById("to_hour" + x).disabled = true;
+		document.getElementById("to_min" + x).disabled = true;
+	}
+
+	$('select#from_hour1').change(function() {
+		document.write($("select#from_hour1").val());
 	});
 });
