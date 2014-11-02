58c5cb4dc68df3fd8898c5a41c73de9e7931ba94
Sahibjot Saggu <sahibjot@live.ca>
2014-11-01 21:38:34 -0400

    Updated code.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	
+	var list = document.getElementById("course_list");
+	$("#add_course").click(function() {
+		var course_code = document.getElementById('course_code').value;
+
+		// error_checking for 'course_code' here:
+
+		
+
+		var entry = document.createElement('li');
+		entry.appendChild(document.createTextNode(course_code));
+		list.appendChild(entry);
+	});
 });
