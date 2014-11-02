93ffb4e59b39f33b70ab2b21ec74bceab1ead19b
Sahibjot Saggu <sahibjot@live.ca>
2014-11-01 22:51:57 -0400

    too tired to write anything useful.

--- script.js
+++ script.js
@@ -10,15 +10,11 @@ $(document).ready(function() {
 		entry.appendChild(document.createTextNode(course_code));
 		list.appendChild(entry);
 	});
-
+/*
 	for (var x = 1; x <= 7; x++) {
 		document.getElementById("from_hour" + x).disabled = true;
 		document.getElementById("from_min" + x).disabled = true;
 		document.getElementById("to_hour" + x).disabled = true;
 		document.getElementById("to_min" + x).disabled = true;
-	}
-
-	$('select#from_hour1').change(function() {
-		document.write($("select#from_hour1").val());
-	});
+	}*/
 });
